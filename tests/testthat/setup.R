aa3efda19a3ea3df3edd6770a1d54fa84444cbdf
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2026-01-20 08:15:06 +0000

    Configure vcr with 30-day cassette expiry

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,3 +3,9 @@ withr::local_envvar(
   .new = c(CONTACTSURVEYS_HOME = td),
   .local_envir = teardown_env()
 )
+
+# Configure vcr with 30-day cassette expiry
+vcr::vcr_configure(
+  dir = testthat::test_path("_vcr"),
+  re_record_interval = 60 * 60 * 24 * 30
+)
